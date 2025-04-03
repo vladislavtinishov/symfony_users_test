83abba82a1b6f10d56d7870dfcc7ff5214d96d8e
vlad <vtinishov@gmail.com>
2025-04-04 04:55:41 +0500

    убрал лишнее

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,16 +2,13 @@
 
 namespace App\Controller;
 
-use App\Dto\LoginRequest;
 use App\Entity\User;
 use App\Traits\ProcessError;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -58,35 +55,5 @@ final class AuthController extends AbstractController
                 'email' => $user->getEmail(),
             ]
         ], 201);
-    }
-
-    #[Route('/api/login', name: 'user_login', methods: ['POST'])]
-    public function login(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
-        JWTTokenManagerInterface $jwtManager,
-        ValidatorInterface $validator
-    ): JsonResponse {
-        $data = json_decode($request->getContent(), true);
-
-        $loginRequest = new LoginRequest($data['email'] ?? null, $data['password'] ?? null);
-        $errors = $validator->validate($loginRequest);
-        if (count($errors) > 0) {
-            return $this->json(['error' => $this->errorsToArray($errors)], 400);
-        }
-
-        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $loginRequest->email]);
-
-        if (!$user || !$passwordHasher->isPasswordValid($user, $loginRequest->password)) {
-            return $this->json(['error' => 'Invalid email or password'], 401);
-        }
-
-        $token = $jwtManager->create($user);
-
-        return $this->json([
-            'message' => 'Login successful',
-            'token' => $token
-        ]);
     }
 }
